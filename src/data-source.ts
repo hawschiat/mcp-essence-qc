c099492de1528b2e30fa3b3e6adf64e6aa0bfa02
Seng Chiat Haw <16750783+hawschiat@users.noreply.github.com>
2026-04-05 18:38:23 -0400

    Sort data by score

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,8 @@
+import * as turf from "@turf/turf"
 import z from "zod";
 import { GeoJSON2DPointSchema, GeoJSONFeatureCollectionGenericSchema } from "zod-geojson";
 import { GeoPointRBush } from "./rbush";
-import { FeaturePropertySchema } from "./types";
+import { FeaturePropertySchema, GasPrices, GasType, GasTypeSchema } from "./types";
 
 let cachedETag: string | undefined = undefined;
 let cachedData: GeoPointRBush | undefined = undefined;
@@ -41,4 +42,76 @@ async function fetchData(): Promise<GeoPointRBush> {
     cachedData = new GeoPointRBush().load(parsedData.features);
 
     return cachedData;
+}
+
+const FindParametersSchema = z.object({
+    radiusKm: z.number(),
+    targetType: z.optional(GasTypeSchema),
+});
+const SortParametersSchema = z.object({
+    order: z.enum(["asc", "desc"]),
+    distanceFactor: z.number().min(0).max(1),
+    priceFactor: z.number().min(0).max(1),
+});
+
+type Coordinates = [number, number];
+type FindParameters = z.infer<typeof FindParametersSchema>;
+type SortParameters = z.infer<typeof SortParametersSchema>;
+
+const defaultSortParams: SortParameters = {
+    order: 'asc',
+    distanceFactor: 0.5,
+    priceFactor: 0.5,
+};
+
+// Helper to normalize (0 to 1)
+function normalize<T extends number>(val: T, min: T, max: T){
+    return (max === min ? 0 : (val - min) / (max - min));
+}
+
+function findGasPrice(prices: GasPrices, targetType: GasType | undefined) {
+    const filtered = prices.filter(p => p.Price !== null);
+    return targetType ?
+        filtered.find(p => p.GasType === targetType)?.Price :
+        Math.min(...(filtered.map(p => p.Price) as number[]));
+}
+
+async function findStationsWithinRadius(pos: Coordinates, findParams: FindParameters, sortParams: SortParameters = defaultSortParams) {
+    const tree = await fetchData();
+    const centerPoint = turf.point(pos);
+
+    const { radiusKm, targetType } = findParams;
+
+    // Create the BBox for the radius
+    // Turf's circle creates a polygon, then we get its bbox
+    const circle = turf.circle(centerPoint, radiusKm, { units: 'kilometers' });
+    const [minX, minY, maxX, maxY] = turf.bbox(circle);
+
+    // returns points in the square
+    const candidates = tree.search({ minX, minY, maxX, maxY });
+
+    const distances = candidates.map(c => turf.distance(centerPoint, c, { units: 'kilometers' }));
+    const prices = candidates.map(c => findGasPrice(c.properties.Prices, targetType))
+        .filter((p): p is number => typeof p === "number");
+
+    const maxD = Math.max(...distances);
+    const minD = Math.min(...distances);
+    const maxP = Math.max(...prices);
+    const minP = Math.min(...distances);
+
+    // Sort by score
+    return candidates
+        .map(c => {
+            const dist = turf.distance(centerPoint, c, { units: 'kilometers' });
+
+            let price = findGasPrice(c.properties.Prices, targetType);
+            price = typeof price === "number" ? price : (sortParams.order === 'asc' ? Infinity : -Infinity);
+
+            const weightedDist = normalize(dist, minD, maxD) * sortParams.distanceFactor;
+            const weightedPrice = normalize(price, minP, maxP) * sortParams.priceFactor;
+
+            return { ...c.properties, distance: dist, score: weightedDist + weightedPrice };
+        })
+        .filter(f => f.distance <= radiusKm)
+        .sort((a, b) => a.score - b.score);
 }
--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,33 @@
 import z from "zod";
 
+const GasPricesSchema = z.array(z.object({
+    GasType: z.string().lowercase(),
+    Price: z.string().transform(s => {
+        const match = s.match(/^(\d|\.)+¢$/);
+        if (match && match[1]) {
+            return Number(match[1]);
+        }
+        return null;
+    }),
+    IsAvailable: z.boolean(),
+}));
+
+export type GasPrices = z.infer<typeof GasPricesSchema>;
+
 export const FeaturePropertySchema = z.object({
-    Name: z.string(),
-    brand: z.string(),
-    status: z.string(),
-    Address: z.string(),
-    PostalCode: z.string(),
-    Region: z.string(),
-    Prices: z.array(z.object({
-        GasType: z.string(),
-        Price: z.string().transform(s => {
-            const match = s.match(/^(\d|\.)+¢$/);
-            if (match && match[1]) {
-                return Number(match[1]);
-            }
-            return null;
-        }),
-        IsAvailable: z.boolean(),
-    }))
+    // normalize the values by lowercasing
+    Name: z.string().lowercase(),
+    brand: z.string().lowercase(),
+    status: z.string().lowercase(),
+    Address: z.string().lowercase(),
+    PostalCode: z.string().lowercase(),
+    Region: z.string().lowercase(),
+    Prices: GasPricesSchema,
 });
+
+export enum GasType {
+    REGULAR = "régulier",
+    PREMIUM = "super",
+    DIESEL = "diesel"
+}
+export const GasTypeSchema = z.enum(Object.values(GasType));
